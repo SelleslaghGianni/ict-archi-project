b572f254c3c79309fcd102b04d03866929a1047c
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2021-12-19 22:14:26 +0000

    add last small fix

--- backend/routes.ts
+++ backend/routes.ts
@@ -24,7 +24,6 @@ export async function getImage (req: Request, res: Response) {
         Key: image.uuid
         };
         s3Client.getObject(params, (err, data) => {
-            console.log(err)
             res.send(data.Body)
         })
     }
